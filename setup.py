f0baedd0cda1e313cec16f1e6736e312de58010a
Travis Swicegood <development@domain51.com>
2012-04-30 16:05:35 -0500

    Initial commit with README and setup.py

---
+++ setup.py
@@ -0,0 +1,39 @@
+from distutils.core import setup
+import os
+
+package_data = []
+BASE_DIR = os.path.dirname(__file__)
+walk_generator = os.walk(os.path.join(BASE_DIR, "project_template"))
+paths_and_files = [(paths, files) for paths, dirs, files in walk_generator]
+for path, files in paths_and_files:
+    prefix = path[path.find("project_template") + len("project_template/"):]
+    if files:
+        package_data.append(os.path.join(prefix, "*.*"))
+
+setup(
+    name="armstrong.templates.paywall",
+    version="1.0alpha.0",
+    description="Provides a basic project template for an Armstrong project",
+    long_description=open("README.rst").read(),
+    author='Texas Tribune & Bay Citizen',
+    author_email='dev@armstrongcms.org',
+    packages=[
+        "armstrong.templates.paywall",
+    ],
+    package_dir={
+        "armstrong.templates.paywall": "project_template",
+    },
+    package_data={
+        "armstrong.templates.paywall": package_data,
+    },
+    namespace_packages=[
+        "armstrong",
+        "armstrong.templates",
+        "armstrong.templates.paywall",
+    ],
+    entry_points={
+        "armstrong.templates": [
+            "paywall = armstrong.templates.paywall",
+        ],
+    },
+)
